0e31bb8fac6f74a15364b204263fb1642c54eb08
Akira Ohgaki <akiraohgaki@gmail.com>
2016-11-07 18:15:45 +0900

    Add app for testing

---
+++ src/app/main.cpp
@@ -0,0 +1,17 @@
+#include <QCoreApplication>
+
+#include "../libs/utils/config.h"
+#include "../libs/utils/network.h"
+#include "../libs/utils/file.h"
+#include "../libs/utils/json.h"
+#include "../libs/utils/package.h"
+#if defined(Q_OS_ANDROID)
+#include "../libs/utils/android.h"
+#endif
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    return app.exec();
+}
